47bfb1adb962b4c878cd219073fc84085b1e2b75
Aruzhan <arukakalhozha1800@gmail.com>
2022-04-25 21:49:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+const carculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
+function sendNumberValue(number) {
+  // Replace current display value if first value is entered
+  if (awaitingNextValue) {
+      carculatorDisplay.textContent = number;
+      awaitingNextValue = false;
+  }  else {
+   // If current display value is 0, replace it, if not add number
+   const displayValue = carculatorDisplay.textContent;
+   carculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+  }
+}
+
+function addDecimal() {
+  // If operator pressed, don't add decimal
+  if (awaitingNextValue) return;
+  // If no decimal, add one 
+  if (!carculatorDisplay.textContent.includes('.')) {
+      carculatorDisplay.textContent = `${carculatorDisplay.textContent}.`;
+  }
+}
+
+// Calculate first and second values depending on operator
+const carculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+
+    '=': (firstNumber, secondNumber) => firstNumber = secondNumber,
+};
+
+function useOperator(operator) {
+  const currentValue = Number(carculatorDisplay.textContent);
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+      operatorValue = operator;
+  return;
+  }   
+  // Assign firstValue if no value
+  if (!firstValue) {
+      firstValue = currentValue;
+  }  else {
+      const carculation = carculate[operatorValue](firstValue, currentValue);
+      carculatorDisplay.textContent = carculation;
+      firstValue = carculation;
+  }
+  // Ready for next value, store operator 
+   awaitingNextValue = true;  
+   operatorValue = operator;
+}
+
+// Add Event Listeners for numbers, operators, decimal buttons
+inputBtns.forEach((inputBtn) => {
+  if(inputBtn.classList.length === 0) {
+      inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains('operator')) {
+      inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
+  } else if (inputBtn.classList.contains('decimal')) {
+      inputBtn.addEventListener('click', () => addDecimal());
+  }
+});  
+
+// Reset all values, display
+function resetAll() {
+  firstValue = 0;
+  operatorValue = '';
+  awaitingNextValue = false;
+  carculatorDisplay.textContent = '0';
+}
+
+// Event Listener
+clearBtn.addEventListener('click', resetAll);
